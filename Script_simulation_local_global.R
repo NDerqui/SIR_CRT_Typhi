fa9ad7a7ef5221ec6014247608a0750833678100
Nieves Derqui <n.derqui-fernandez@imperial.ac.uk>
2022-10-26 16:48:10 +0100

    Only the observed incident infections for the RRs

--- Script_simulation_local_global.R
+++ Script_simulation_local_global.R
@@ -643,6 +643,10 @@ main <- function(N, C, sd, random_cluster = 1,  # Population and cluster charact
     mutate(sum_SI = sum(inc_sus_inf, na.rm = TRUE)) %>%
     mutate(sum_VI = sum(inc_vax_inf, na.rm = TRUE)) %>%
     mutate(sum_all_inc = sum_SI + sum_VI) %>%
+    # Get only the observed
+    mutate(sum_SI = sum_SI*mu) %>%
+    mutate(sum_VI = sum_VI*mu) %>%
+    mutate(sum_all_inc = sum_all_inc*mu) %>%
     # Only one obs per run and cluster
     filter(row_number() == 1) %>%
     select(-time_seq, -cluster, -eliminate, -susceptible, -vaccinated,
